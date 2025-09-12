5961cb07a2f6a0d61bfa374e9c1056258b4a892d
SilentArctic <silentarcticdev@gmail.com>
2025-09-12 00:14:43 -0500

    add chain icon

--- src/utils/transformDescription.js
+++ src/utils/transformDescription.js
@@ -1,15 +1,22 @@
+import chain from '@/assets/chain.png';
+
 const replacers = {
    reference: /{([^}]+)}/g,
    keyword: /\[([^\]]+)\]/g,
    italic: /_([^_]+)_/g,
    bold: /\*([^\*]+)\*/g,
+   chain: /(\([Cc]\))/g
 };
+
+const chainSize = window.innerWidth <= 750 ? '4vw' : '2vw';
+const chainPos = window.innerWidth <= 750 ? '7px' : '10px';
 
 const styles = {
    reference: 'color: #666ffa',
-   keyword: 'color: #e1c341',
+   keyword: 'color: #f1cf57',
    italic: 'font-style: italic',
    bold: 'font-weight: 600',
+   chain: `width: ${chainSize}; display: inline; transform: translateY(${chainPos})`,
 };
 
 export default function (description) {
@@ -19,4 +26,5 @@ export default function (description) {
       .replace(replacers.keyword, `<span style="${styles.keyword}">$1</span>`)
       .replace(replacers.italic, `<span style="${styles.italic}">$1</span>`)
       .replace(replacers.bold, `<span style="${styles.bold}">$1</span>`)
+      .replace(replacers.chain, `<img style="${styles.chain}" src="${chain}" alt="chain icon" />`);
 }
